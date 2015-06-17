c781ad54c12f70301c05f3e7de602e210e126f02
Parker Miller <despondentdonkey@gmail.com>
2015-06-17 17:21:26 -0600

    Adding level switching

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class GameManager : MonoBehaviour {
+
+    public Level[] availableLevels;
+
+    Level currentLevel = null;
+
+    void Awake() {
+        //setCurrentLevel(availableLevels[1]);
+        setCurrentLevel("testLevel");
+    }
+
+    public void setCurrentLevel(Level level) {
+        if (!availableLevels.Contains(level)) return;
+        this.currentLevel = level;
+        Instantiate(level);
+    }
+
+    public void setCurrentLevel(string levelName) {
+        foreach (Level level in availableLevels) {
+            if (level.name == levelName) {
+                setCurrentLevel(level);
+                return;
+            }
+        }
+        Debug.LogError("Level '" + levelName + "' not found.");
+    }
+
+}
