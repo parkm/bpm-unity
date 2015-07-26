c1dba893b90d181ff38a784b302880c3d75d85b0
dogpantz <dogpantz@gmail.com>
2015-07-26 13:13:19 -0600

    Script Organization
    Added Bubble and Utility folders
    Removed Level.cs (don't think we need this)
    Added FireElement.cs

---
+++ Assets/scripts/FireElement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireElement : MonoBehaviour {
+    public GameObject parentBubble;
+    public int fireDamage;
+
+    void Update() {
+    }
+}
