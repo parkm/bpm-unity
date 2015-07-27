963714fda5d044ea3baf4c071a21335447307ed1
Parker Miller <despondentdonkey@gmail.com>
2015-07-26 21:01:05 -0600

    Added quest asset creator and ScriptableObject.

---
+++ Assets/res/quests/Quest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest : ScriptableObject {
+    public string id;
+    public string questName;
+    [TextArea()]
+    public string description;
+    public int rewardXp;
+
+    [System.Serializable]
+    public class Detail {
+        public enum DetailNames {Multiplier, PopBubbles};
+        public DetailNames detailName;
+        public string value;
+    };
+    public Detail[] details;
+}
---
+++ Assets/res/quests/editor/QuestAsset.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class QuestAsset {
+    [MenuItem("Assets/Create/Quest")]
+    public static void CreateAsset() {
+        ScriptableObjectUtility.CreateAsset<Quest>();
+    }
+}
