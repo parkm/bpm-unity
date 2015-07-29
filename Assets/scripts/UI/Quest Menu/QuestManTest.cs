c54d42823344b1cd6e442bc65fa65798ff5d13c1
Parker Miller <despondentdonkey@gmail.com>
2015-07-28 23:26:47 -0600

    Basic quest menu
    Does not show real quests yet.

---
+++ Assets/scripts/UI/Quest Menu/QuestManTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuestManTest : MonoBehaviour {
+
+    public Transform questsPanel;
+
+    public Transform questButton;
+    public TitleButtonTest titleButton;
+
+    public class Area {
+        public string[] quests;
+        public string name;
+        public Area(string areaName, string[] quests) {
+            this.name = areaName;
+            this.quests = quests;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Area[] areas = new Area[]{
+            new Area("Town", new string[]{"Pop Bubs", "Pop Quiz"}),
+            new Area("Forest", new string[]{"Pop Forest Bubs", "In the Forest", "Forest Fight"}),
+            new Area("Bubble Land", new string[]{"Pop Bubble Land Bubs", "The Land Of Bubs"})
+        };
+
+        foreach (Area a in areas) {
+            Debug.Log(a.name);
+            Transform titleButton = Instantiate(this.titleButton.transform);
+            titleButton.GetComponent<TitleButtonTest>().quests = a.quests;
+            titleButton.Find("Text").GetComponent<Text>().text = a.name;
+            titleButton.SetParent(questsPanel);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/scripts/UI/Quest Menu/TitleButtonTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleButtonTest : MonoBehaviour {
+    public Transform questDropDownPrefab;
+    public Transform questButton;
+
+    public string[] quests;
+
+    private Transform questDropDown;
+
+    public void OnClick() {
+        Debug.Log("Clicked Title Button");
+        if  (this.questDropDown == null) {
+            Transform dropDown = Instantiate(this.questDropDownPrefab);
+
+            dropDown.SetParent(this.transform.parent);
+            dropDown.SetSiblingIndex(this.transform.GetSiblingIndex()+1);
+            this.questDropDown = dropDown;
+
+            foreach (string questName in quests) {
+                Transform questButton = Instantiate(this.questButton);
+                questButton.Find("Text").GetComponent<Text>().text = questName;
+                questButton.SetParent(dropDown);
+            }
+        } else {
+            this.questDropDown.gameObject.SetActive(!questDropDown.gameObject.activeSelf);
+        }
+    }
+
+}
