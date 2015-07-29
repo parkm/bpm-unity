861e66d37612cbe4fb4c57333fd22e9719a1f394
dogpantz <dogpantz@gmail.com>
2015-07-28 19:45:00 -0600

    Added Element Controller and initial work on Fire Element

---
+++ Assets/scripts/ElementController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementController : MonoBehaviour {
+    public Transform fire;
+    public Transform ice;
+    public Transform lightning;
+
+    void Start() {
+        CreateFire();
+    }
+
+    private void CreateFire() {
+        fire.GetComponent<FireElement>().parentBubble = transform.parent.gameObject.GetComponent<Bubble>();
+        Instantiate(fire, transform.position, transform.rotation);
+    }
+}
--- Assets/scripts/FireElement.cs
+++ Assets/scripts/FireElement.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 public class FireElement : MonoBehaviour {
-    public GameObject parentBubble;
+    public Bubble parentBubble;
     public int fireDamage;
 
     void Update() {
+        if (parentBubble == null) {
+            Destroy(this.gameObject);
+        } else {
+            Vector3 parentPos = parentBubble.transform.position;
+            transform.position = new Vector3(parentPos.x, parentPos.y, transform.position.z);
+            parentBubble.health -= fireDamage;
+        }
     }
 }
