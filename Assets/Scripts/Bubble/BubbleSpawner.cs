d268ea6fde96fc010afc2d4616e875af9f6cef4c
Parker Miller <despondentdonkey@gmail.com>
2015-10-19 04:04:12 -0600

    The bubble spawner can now spawn waves of objects

--- Assets/Scripts/Bubble/BubbleSpawner.cs
+++ Assets/Scripts/Bubble/BubbleSpawner.cs
@@ -8,30 +8,54 @@ public class BubbleSpawner : MonoBehaviour {
     public int minHealth = 1;
     public int maxHealth = 20;
 
+    [System.Serializable]
+    public class Wave {
+        public GameObject objectToSpawn;
+        public float spawnAtTime;
+        public int amountToSpawn;
+
+        [System.NonSerialized]
+        public bool spawned = false;
+    }
+    public Wave[] waves;
+
+    public float waveTimer = 0;
+
     MeshRenderer mesh;
 
-    // Use this for initialization
     void Start () {
-        mesh = this.GetComponent<MeshRenderer>();
-        Debug.Log(this.transform.position);
-        Debug.Log(mesh.bounds.min);
-        Debug.Log(mesh.bounds.max);
+        this.mesh = this.GetComponent<MeshRenderer>();
+        this.waveTimer = 0;
 
-        Invoke("spawn", spawnTime);
+        Invoke("Spawn", spawnTime);
     }
 
-    // Update is called once per frame
     void Update () {
-
+        this.waveTimer += Time.deltaTime;
+        foreach (Wave wave in this.waves) {
+            if (wave.spawned) continue;
+            if (this.waveTimer >= wave.spawnAtTime) {
+                this.SpawnWave(wave);
+                wave.spawned = true;
+            }
+        }
     }
 
-    void spawn() {
+    void Spawn() {
         Bubble bubble = Instantiate(this.bubble);
         Vector3 min = mesh.bounds.min;
         Vector3 max = mesh.bounds.max;
         bubble.transform.position = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
         bubble.maxHealth = Random.Range(minHealth, maxHealth);
-        //Debug.Log (bubble.transform.position);
-        Invoke("spawn", spawnTime);
+        Invoke("Spawn", spawnTime);
+    }
+
+    void SpawnWave(Wave wave) {
+        Vector3 min = mesh.bounds.min;
+        Vector3 max = mesh.bounds.max;
+        for (int i=0; i<wave.amountToSpawn; ++i) {
+            GameObject spawn = (GameObject) Instantiate(wave.objectToSpawn);
+            spawn.transform.position = new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
+        }
     }
 }
