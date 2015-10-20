824904aa0eb104db8fa4cb750a5682b691490da5
Parker Miller <despondentdonkey@gmail.com>
2015-10-20 05:43:34 -0600

    Added quest fail and timer ui

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,7 +4,11 @@ using System.Collections;
 using System;
 
 public class LevelManager : MonoBehaviour {
+    public GameObject onQuestEndUi;
     public GameObject onQuestCompleteUi;
+    public GameObject onQuestFailUi;
+    public GameObject timePanelUi;
+    public Text timeTextUi;
 
     public event Action OnQuestTimerComplete = delegate() {};
 
@@ -22,6 +26,9 @@ public class LevelManager : MonoBehaviour {
             if (this.quest != null) {
                 quest.Start(this);
                 this.questTime = quest.asset.time;
+                if (this.questTime > 0) {
+                    timePanelUi.SetActive(true);
+                }
             }
             questMan.OnQuestComplete += OnQuestComplete;
             questMan.OnQuestFail += OnQuestFail;
@@ -31,6 +38,7 @@ public class LevelManager : MonoBehaviour {
     void Update() {
         if (this.questTime > 0) {
             this.questTimer += Time.deltaTime;
+            this.timeTextUi.text = Mathf.Ceil(this.questTime - this.questTimer).ToString();
             if (this.questTimer >= this.questTime) {
                 OnQuestTimerComplete();
                 if (!this.quest.IsComplete()) {
@@ -52,13 +60,13 @@ public class LevelManager : MonoBehaviour {
 
     void OnQuestComplete(Quest quest) {
         Debug.Log("quest complete!");
-        this.onQuestCompleteUi.SetActive(true);
-
+        this.ShowQuestEndUi(false);
         questMan.UnlockNewQuests(questMan.CurrentQuest);
     }
 
     void OnQuestFail(Quest quest) {
         Debug.Log("quest failed");
+        this.ShowQuestEndUi(true);
     }
 
     public void ExitLevel() {
@@ -67,5 +75,14 @@ public class LevelManager : MonoBehaviour {
         } else {
             CutsceneManager.ShowCutscene(this.quest.asset.endingCutscene, "questMenu");
         }
+    }
+
+    void ShowQuestEndUi(bool failed) {
+        this.onQuestEndUi.SetActive(true);
+        if (failed)
+            this.onQuestFailUi.SetActive(true);
+        else
+            this.onQuestCompleteUi.SetActive(true);
+
     }
 }
