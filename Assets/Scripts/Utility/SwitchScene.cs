19b693c05b3c8c9d9f20459b63bd151a05b258e9
Parker Miller <despondentdonkey@gmail.com>
2015-11-29 12:42:10 -0700

    All scenes should initiate from the Main scene
    Change what scene Main will go to in the SwitchScene prefab.

---
+++ Assets/Scripts/Utility/SwitchScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScene : MonoBehaviour {
+    public Object scene;
+
+    void Start() {
+        Application.LoadLevel(scene.name);
+    }
+
+}
